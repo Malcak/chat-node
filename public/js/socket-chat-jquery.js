d09761e6a19e569e600b8f485d0791ddeee943ad
Malcak <Malcakx@gmail.com>
2020-04-23 17:48:06 -0500

    Chat funcional, se agregó estilo

---
+++ public/js/socket-chat-jquery.js
@@ -0,0 +1,116 @@
+var params = new URLSearchParams(window.location.search);
+
+var nombre = params.get('nombre');
+var sala = params.get('sala');
+
+// referencias de jQuery
+var divUsuarios = $('#divUsuarios');
+var formEnviar = $('#formEnviar');
+var txtMensaje = $('#txtMensaje');
+
+var divChatbox = $('#divChatbox');
+
+
+
+// Funcione spara renderizar usuarios
+function renderizarusuarios(usuarios) { // [{}, {}, {}]
+	console.log(usuarios);
+
+	var html = '';
+	html += '<li>';
+	html += '    <a href="javascript:void(0)" class="active"> Chat de <span>' + params.get('sala') + '</span></a>';
+	html += '</li>';
+
+	for (let i = 0; i < usuarios.length; i++) {
+		const usuario = usuarios[i];
+
+		html += '<li>';
+		html += '    <a data-id="' + usuario.id + '" href="javascript:void(0)"><img src="assets/images/users/1.jpg" alt="user-img" class="img-circle"> <span>' + usuario.nombre + '<small class="text-success">online</small></span></a>';
+		html += '</li>';
+	}
+
+	divUsuarios.html(html);
+
+}
+
+function renderizarMensajes(mensaje, yo) {
+
+	var html = '';
+	var fecha = new Date(mensaje.fecha);
+	var hora = fecha.getHours() + ':' + fecha.getMinutes();
+
+	var adminClass = 'info';
+	if (mensaje.nombre === 'Servidor') {
+		adminClass = 'danger';
+	}
+
+	if (yo) {
+		html += '<li class="reverse animated fadeIn">';
+		html += '	<div class="chat-content">';
+		html += '		<h5>' + mensaje.nombre + '</h5>';
+		html += '		<div class="box bg-light-inverse">' + mensaje.mensaje + '</div>';
+		html += '	</div>';
+		html += '	<div class="chat-img"><img src="assets/images/users/5.jpg" alt="user" />';
+		html += '	</div>';
+		html += '	<div class="chat-time">' + hora + '</div>';
+		html += '</li>';
+	} else {
+		html += '<li class="animated fadeIn">';
+		if (mensaje.nombre !== 'Servidor') {
+			html += '	<div class="chat-img"><img src="assets/images/users/1.jpg" alt="user" />';
+		}
+		html += '	</div>';
+		html += '	<div class="chat-content">';
+		html += '		<h5>' + mensaje.nombre + '</h5>';
+		html += '		<div class="box bg-light-' + adminClass + '">' + mensaje.mensaje + '</div>';
+		html += '	</div>';
+		html += '	<div class="chat-time">' + hora + '</div>';
+		html += '</li>';
+	}
+
+	divChatbox.append(html);
+}
+
+function scrollBottom() {
+
+	// selectors
+	var newMessage = divChatbox.children('li:last-child');
+
+	// heights
+	var clientHeight = divChatbox.prop('clientHeight');
+	var scrollTop = divChatbox.prop('scrollTop');
+	var scrollHeight = divChatbox.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight() || 0;
+
+	if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+		divChatbox.scrollTop(scrollHeight);
+	}
+}
+
+// listeners
+divUsuarios.on('click', 'a', function () {
+	var id = $(this).data('id');
+
+	if (id) {
+		console.log(id);
+	}
+});
+
+formEnviar.on('submit', function (e) {
+	e.preventDefault();
+
+	if (txtMensaje.val().trim().length === 0) {
+		return;
+	}
+
+	socket.emit('crearMensaje', {
+		nombre: nombre,
+		mensaje: txtMensaje.val()
+	}, function (mensaje) {
+		txtMensaje.val('').focus();
+		renderizarMensajes(mensaje, true);
+		scrollBottom();
+	});
+
+});
--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -4,7 +4,7 @@ var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre y la Sala son necesarios');
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
@@ -12,40 +12,51 @@ var usuario = {
     sala: params.get('sala')
 };
 
+
+
 socket.on('connect', function () {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function (resp) {
-        console.log('usuarios conectados: ', resp);
-
+        // console.log('Usuarios conectados', resp);
+        renderizarusuarios(resp);
     });
+
 });
 
+// escuchar
 socket.on('disconnect', function () {
 
     console.log('Perdimos conexión con el servidor');
 
 });
 
+
+// Enviar información
 // socket.emit('crearMensaje', {
-//     usuario: 'Fernando',
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
-// }, function (resp) {
+// }, function(resp) {
 //     console.log('respuesta server: ', resp);
 // });
 
 // Escuchar información
 socket.on('crearMensaje', function (mensaje) {
-    console.log('Servidor:', mensaje);
+    // console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-// Escuchar cambios de un usuario
+// Escuchar cambios de usuarios
 // cuando un usuario entra o sale del chat
-socket.on('listaPersona', function (usuarios) {
-    console.log('Servidor:', usuarios);
+socket.on('listaPersona', function (personas) {
+    // console.log(personas);
+    renderizarusuarios(personas);
 });
 
 // Mensajes privados
 socket.on('mensajePrivado', function (mensaje) {
-    console.log('Mensaje prievado:', mensaje);
+
+    console.log('Mensaje Privado:', mensaje);
+
 });
